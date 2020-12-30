191abc83cf1c610ecbc9017ef5fa4a63d591e612
AnoushiravanR <72711779+AnoushiravanR@users.noreply.github.com>
2020-12-31 00:48:19 +0330

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,8 @@
 library(testthat)
 
 
-test_that("printing file name", {
-  expect_that(make_filename(2013), prints_text())
-})
-
 test_that("erroneous file name", {
   expect_that(fars_read("accident_2012.csv.bz2"), throws_error())
-})
-
-test_that("invalid years", {
-  expect_that(fars_read_years(c(2010, 2011)), gives_warning())
 })
 
 
